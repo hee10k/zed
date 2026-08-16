1ad2408e535db94dd4b39cbaf30a95b19cd1aed7
황희만 <dodev@naver.com>
2026-08-16 10:54:21 +0900

    fix(build): work around MSVC CXX trait lookup

---
+++ vendor/cxx-build/src/gen/builtin/shared_ptr.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../../../include/cxx.h"
+#include <memory>
+
+namespace rust {
+inline namespace cxxbridge1 {
+namespace detail {
+template <typename T, bool = ::rust::detail::is_complete<T>::value>
+struct is_destructible : ::std::false_type {};
+//
+template <typename T>
+struct is_destructible<T, true> : ::std::is_destructible<T> {};
+//
+template <typename T>
+struct is_destructible<T[], false> : is_destructible<T> {};
+} // namespace detail
+namespace {
+template <typename T, bool = ::rust::cxxbridge1::detail::is_destructible<T>::value>
+struct shared_ptr_if_destructible {
+  explicit shared_ptr_if_destructible(typename ::std::shared_ptr<T>::element_type *) {}
+};
+//
+template <typename T>
+struct shared_ptr_if_destructible<T, true> : ::std::shared_ptr<T> {
+  using ::std::shared_ptr<T>::shared_ptr;
+};
+} // namespace
+} // namespace cxxbridge1
+} // namespace rust
