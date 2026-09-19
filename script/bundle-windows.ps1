7a3db5039874bff179c5b9498273b0e0d7872675
황희만 <dodev@naver.com>
2026-09-20 01:33:39 +0900

    fix(windows): bundle working ConPTY runtime

--- script/bundle-windows.ps1
+++ script/bundle-windows.ps1
@@ -243,8 +243,8 @@ function DownloadAMDGpuServices {
 }
 
 function DownloadConpty {
-    $url = "https://github.com/microsoft/terminal/releases/download/v1.23.13503.0/Microsoft.Windows.Console.ConPTY.1.23.251216003.nupkg"
-    $zipPath = ".\Microsoft.Windows.Console.ConPTY.1.23.251216003.nupkg"
+    $url = "https://github.com/microsoft/terminal/releases/download/v1.24.10621.0/Microsoft.Windows.Console.ConPTY.1.24.260303001.nupkg"
+    $zipPath = ".\Microsoft.Windows.Console.ConPTY.1.24.260303001.nupkg"
     Invoke-WebRequest -Uri $url -OutFile $zipPath
     Expand-Archive -Path $zipPath -DestinationPath ".\conpty" -Force
 }
